aa21bbcfcc5bde3333d7d739951a044aadcbe0f3
Dirk Arnez <smalldirkalex@gmail.com>
2019-01-02 08:02:58 +0800

    - update index.js

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
 (function () {
    window.singleWindows = window.singleWindows || {};
 
+   window.localSingleWindowsClose = window.localSingleWindowsClose || function(types) {
+      for (var type of types) {
+         var w = window.singleWindows[type];
+         if (w && !w.closed) {
+            w.close();
+         }
+         delete window.singleWindows[type];
+      }
+   }
+
+   window.handleStorageChange = window.handleStorageChange || function(event) {
+      if (event.key != "type") {
+         return;
+      }
+
+      var data = event.newValue;
+      var typesString = data.match(/\d+:(.*)/)[1];
+      var types = typesString.split(/\s*,\s*/);
+
+      if (types.length == 1 && types[0].length == 0) {
+         types = Object.keys(window.singleWindows);
+      }
+      
+      window.localSingleWindowsClose(types);
+   };
+
+   window.addEventListener('storage', window.handleStorageChange);
+
+   window.handleWindowUnload = window.handleWindowUnload || function (event) {
+      window.singleWindowClose();
+   }
+
+   window.addEventListener("unload", window.handleWindowUnload);
+
    window.singleWindowOpen = window.singleWindowOpen || function (url, type) {
       var w = window.singleWindows[type];
       if (w == undefined || w == null || w.closed) {
@@ -10,11 +44,18 @@
       }
    };
 
-   window.singleWindowClose = window.singleWindowClose || function (type) {
-      var w = window.singleWindows[type];
-      if (w && !w.closed) {
-         w.close();
+   window.singleWindowClose = window.singleWindowClose || function () {
+      var hasStorageFeature = typeof (Storage) !== "undefined";
+
+      var types = [];
+      if (arguments.length > 0) {
+         types = Array.from(arguments);
+         hasStorageFeature && localStorage.setItem("type", 1 * new Date() + ":" + types.join(","));
+      } else {
+         types = Object.keys(window.singleWindows);
+         hasStorageFeature && localStorage.setItem("type", 1 * new Date() + ":");
       }
-      delete window.singleWindows[type];
+
+      window.localSingleWindowsClose(types);
    }
 }());
